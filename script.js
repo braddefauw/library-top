23940a7f1476c5c5cff3e08a39cb1261c5e19036
braddefauw <badefauw@gmail.com>
2022-04-08 15:42:34 -0500

    placeholder form inserted on click of add book button. user can close out by clicking x

--- script.js
+++ script.js
@@ -11,6 +11,37 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
+function show(){
+  document.getElementById("popup").style.display = "block";
+}
+function hide() {
+  document.getElementById("popup").style.display = "none";
+  
+  document.getElementById("name").value = "";
+  document.getElementById("age").value = "";
+  document.getElementById("country").value = "";
+}
+
+function add() {
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var country = document.getElementById("country").value;
+  
+  if (name == "" || age == "" || country == "") {
+    alert("Please fill all fields.")
+  } else {
+    document.getElementById("popup").style.display = "none";
+    var newdiv = document.createElement("div");
+    newdiv.className += "cont";
+    newdiv.innerHTML = "Name: "+ name + "<br>Age: " + age + "<br>Country: " + country;
+    document.getElementById("results").appendChild(newdiv);
+    
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("country").value = "";
+  }
+}
+
 function addBookToLibrary() {
   // content to be added
 }
