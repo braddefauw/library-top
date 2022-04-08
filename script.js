0827208c9bf703ba19271e7cf43fb6d8068f38ff
braddefauw <badefauw@gmail.com>
2022-04-08 10:59:34 -0500

    loading in js dummy content + started css alignment

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello world")
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read;
+}
+
+function addBookToLibrary() {
+  const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Read');
+  const pachinko = new Book('Pachinko', 'Min Jin Liee', 496, 'Unread');
+  myLibrary.push(theHobbit, pachinko);
+  console.log(myLibrary);
+}
+
+addBookToLibrary()
+document.getElementById("title").innerText = myLibrary[0].title;
+document.getElementById("author").innerText = myLibrary[0].author;
+document.getElementById("pages").innerText = `${myLibrary[0].pages} pages`;
+document.getElementById("read").innerText = myLibrary[0].read;
