654a48170855eb0dc442249812495daabefa65d2
braddefauw <badefauw@gmail.com>
2022-04-08 13:14:41 -0500

    dynamically made cards from js + updated add book to button

--- script.js
+++ script.js
@@ -16,7 +16,33 @@ function addBookToLibrary() {
 
 addBookToLibrary()
 
-function makeNewCard(){}
+function makeNewCard(){
+  for(const obj of myLibrary){
+    let cards = document.getElementById("cards");
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let cardTitle = document.createElement("p");
+    cardTitle.innerText = obj.title;
+    card.appendChild(cardTitle);
+
+    let cardAuthor = document.createElement("p");
+    cardAuthor.innerText = obj.author;
+    card.appendChild(cardAuthor);
+
+    let cardPages = document.createElement("p");
+    cardPages.innerText = `${obj.pages} pages`;
+    card.appendChild(cardPages);
+
+    let cardRead = document.createElement("p");
+    cardRead.innerText = obj.read;
+    card.appendChild(cardRead);
+
+    cards.appendChild(card);
+  }
+}
+
+makeNewCard();
 
 document.getElementById("title").innerText = `"${myLibrary[0].title}"`;
 document.getElementById("author").innerText = myLibrary[0].author;
